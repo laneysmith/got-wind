21cf553b8202d7f361dc0a7ac4bbe7b9af359c88
Laney Smith <lsmith.unc@gmail.com>
2016-09-16 10:31:02 -0600

    Complete animated wind lines, need to rotate

--- README.md
+++ README.md
@@ -8,7 +8,11 @@ ogr2ogr -f GeoJSON states.json cb_2015_us_state_500k.shp
 ```
 
 #### Meteorological data
-[grib2json](https://github.com/cambecc/grib2json)
+GFS 0.25 degree data downloaded from [NOAA NOMADS](http://nomads.ncep.noaa.gov/) and converted to JSON using [grib2json](https://github.com/cambecc/grib2json) utility:
+```
+curl "http://nomads.ncep.noaa.gov/cgi-bin/filter_wave.pl?file=nah.t00z.grib.grib2&lev_surface=on&var_WIND=on&var_WVDIR=on&var_WVHGT=on&var_WVPER=on&subregion=&leftlon=-81&rightlon=-70&toplat=40&bottomlat=30&dir=%2Fmulti_2.20160912" -o waveData.grb2
+grib2json -d -n -o waveData.json waveData.grb2
+```
 
 #### Helpful links
 * [Let's Make a Map](https://bost.ocks.org/mike/map/)
--- js/d3.map.js
+++ js/d3.map.js
@@ -42,17 +42,227 @@ svg
 
 
 
+// console.log(waveData);
+var waveHeight = waveData[3].data;
+var headers = waveData[3].header;
+var la1 = headers.la1;
+var lo1 = headers.lo1;
+var dx = headers.dx;
+var dy = headers.dy;
+var nx = headers.nx;
+
+var mydata = waveHeight.map(function(d, i) {
+  var pos = projection([
+    lo1 + (i * dx),
+    la1 + (i * dy)
+  ]);
+  return {
+    t: d,
+    x: pos[0],
+    y: pos[1]
+  };
+})
+
+// var mydata = waveHeight.map(function(d, i) {
+//   var pos = projection([
+//     lo1 + dx * (i % nx),
+//     la1 + dy * parseInt(i / nx)
+//   ]);
+//   return {
+//     t: d,
+//     x: pos[0],
+//     y: pos[1]
+//   };
+// })
+// mydata.forEach(function(d) {
+//   g.append("path")
+//       .datum({type: "Point", coordinates: [d.x, d.y]})
+//       .attr("d", path.pointRadius(d.t));
+// });
+
+// console.log("mydata=", mydata);
+// var color = d3.scale.linear()
+//   .domain(d3.extent(waveHeight))
+//   .range(["blue", "red"]);
+// var waveCircles = g.append('g').attr('class', 'temperatures');
+// waveCircles.selectAll('circle')
+//   .data(mydata.filter(function(d) {
+//     if (isNaN(d.t)) {
+//       return false;
+//     } else {
+//       return true;
+//     };
+//   }))
+//   .enter()
+//   .append('rect')
+//   .on("click", clicked)
+//   // .attr('r', 30)
+//   .attr({
+//     x: function(d) { return d.x;},
+//     y: function(d) { return d.y;},
+//     width: nx,
+//     height: nx
+//   })
+//   // .attr('cx', function(d) {
+//   //   return d.x;
+//   // })
+//   // .attr('cy', function(d) {
+//   //   return d.y;
+//   // })
+//   .style('fill', function(d) {
+//     return color(d.t)
+//   });
 
 
-  // console.log('windData', windData);
-  // var wind = windData.dwml.data;
-  // var windInfo = windData.dwml.head;
-  // var la1 = headers.la1;
-  // var lo1 = headers.lo1;
-  // var dx = headers.dx;
-  // var dy = headers.dy;
-  // var nx = headers.nx;
+
+
+
+
+
+
+d3.json("maps/states.json", function(error, us) {
+  if (error) throw error;
+  // add states to map
+  g.selectAll("path")
+    .data(us.features)
+    .enter()
+    .append("path")
+    .attr("class", "land")
+    .style("stroke-width", "0.5px")
+    .attr("d", path)
+    .on("click", clicked);
+
+  g.selectAll(".pin")
+    .data(cities)
+    .enter().append("circle", ".pin")
+    .attr("r", 2)
+    .attr("transform", function(d) {
+      return "translate(" + projection([
+        d.lon,
+        d.lat
+      ]) + ")";
+    })
+    .text(function(d) {
+      return d.city;
+    });
+
+  // .style("text-anchor", function(d) {
+  // 	return d.lon > -1 ? "start" : "end";
+  // });
+
+  // g.append("path")
+  // 	.datum(topojson.feature(windData, windData.dwml.data))
+  // 	.attr("d", path)
+  // 	.attr("class", "place");
+
+
+
+
+
+
+
+  // WIND VECTORS
+
+  console.log(dwmlThingy);
+  var wind = dwmlThingy.data;
+  var len = wind.location.length;
+  var windInfo = dwmlThingy.head;
+  var windLocation = wind.location;
+  var windParameters = wind.parameters;
+  var windArray = [];
+  for (let i = 0; i < len; i++) {
+    var dataPoint = {
+      lat: parseFloat(windLocation[i].point["latitude"]),
+      lon: parseFloat(windLocation[i].point["longitude"]),
+      dir: windParameters[i].direction.value,
+      spd: windParameters[i]["wind-speed"].value
+    }
+    windArray.push(dataPoint)
+  }
+
+  var windColor = d3.scale.linear()
+    .domain([0, 25])
+    .range(["blue", "red"]);
+  g.selectAll(".thing")
+    .data(windArray)
+    .enter()
+    .append("line")
+    .attr({
+      x1: function(d) {
+        return d.lat
+      },
+      y1: function(d) {
+        return d.lon
+      },
+      x2: function(d) {
+        return (d.lat + (parseInt(d.spd[0])))
+      },
+      y2: function(d) {
+        return (d.lon + (parseInt(d.spd[0])))
+      },
+    })
+    .attr("stroke-width", 2)
+    .attr("stroke", function(d) {
+      return windColor(parseInt(d.spd[0]))
+    })
+    .on("click", clicked)
+    .attr("transform", function(d) {
+      return "translate(" + projection([
+        d.lon,
+        d.lat
+      ]) + ")";
+    })
+    // .attr('transform', 'translate(200,100)rotate(-22)')
+    // .rotate(72)
+    .text(function(d) {
+      return d.spd;
+    })
+    .call(lineAnimate);
+
+
+  // var color = d3.scale.linear()
+  //   .domain(d3.extent(windArray))
+  //   .range(["white", "black"]);
+  // var tempCircles = g.append('g').attr('class', 'temperatures');
+  // tempCircles.selectAll('line')
+  //   .data(windArray.filter(function(d) {
+  //     if (isNaN(d.spd)) {
+  //       return false;
+  //     } else {
+  //       return true;
+  //     };
+  //   }))
+  //   .enter()
+  //   .append("line")
+  //   .attr({
+  //     x1: function(d) {return d.lat},
+  //     y1: function(d) {return d.lon},
+  //     x2: function(d) {return (d.lat+(5*d.spd))},
+  //     y2: function(d) {return (d.lon+(5*d.spd))},
+  //   })
+  //   .attr("stroke-width", 2)
+  //   .attr("stroke", "red")
+  //   .on("click", clicked)
+
+
+
+
   //
+  // g.selectAll("circle")
+  // .data(windArray).enter()
+  // .append("circle")
+  // .attr("cx", function (d) { console.log(projection(d)); return projection(d.lon)[0]; })
+  // .attr("cy", function (d) { return projection(d.lat)[0]; })
+  // .attr("r", "8px")
+  // .attr("fill", "red")
+
+
+  // windArray.forEach(function(d) {
+  //   g.append("path")
+  //       .datum({type: "Point", coordinates: [d.lat, d.lon]})
+  //       .attr("d", path.pointRadius(d.spd[0]));
+  // });
+
   // var myWidnData = wind.map(function(d, i) {
   //   var pos = projection([
   //     lo1 + dx * (i % nx),
@@ -97,92 +307,7 @@ svg
 
 
 
-
-	console.log(waveData);
-  var waveHeight = waveData[3].data;
-  var headers = waveData[3].header;
-  var la1 = headers.la1;
-  var lo1 = headers.lo1;
-  var dx = headers.dx;
-  var dy = headers.dy;
-  var nx = headers.nx;
-
-  var mydata = waveHeight.map(function(d, i) {
-    var pos = projection([
-      lo1 + dx * (i % nx),
-      la1 + dy * parseInt(i / nx)
-    ]);
-    return {
-      t: d,
-      x: pos[0],
-      y: pos[1]
-    };
-  })
-  var color = d3.scale.linear()
-    .domain(d3.extent(waveHeight))
-    .range(["blue", "red"]);
-  var waveCircles = g.append('g').attr('class', 'temperatures');
-  waveCircles.selectAll('circle')
-    .data(mydata.filter(function(d) {
-      if (isNaN(d.t)) {
-        return false;
-      } else {
-        return true;
-      };
-    }))
-    .enter()
-    .append('rect')
-    .on("click", clicked)
-    // .attr('r', 30)
-    .attr({
-      x: function(d) { return d.x;},
-      y: function(d) { return d.y;},
-      width: nx,
-      height: nx
-    })
-    // .attr('cx', function(d) {
-    //   return d.x;
-    // })
-    // .attr('cy', function(d) {
-    //   return d.y;
-    // })
-    .style('fill', function(d) {
-      return color(d.t)
-    });
-
-
-
-
-
-
-
-
-
-d3.json("maps/states.json", function(error, us) {
-  if (error) throw error;
-  // add states to map
-  g.selectAll("path")
-    .data(us.features)
-    .enter()
-    .append("path")
-    .attr("class", "land")
-    .style("stroke-width", "0.5px")
-    .attr("d", path)
-    .on("click", clicked);
-
-  g.selectAll(".pin")
-    .data(cities)
-    .enter().append("circle", ".pin")
-    .attr("r", 2)
-    .attr("transform", function(d) {
-      return "translate(" + projection([
-        d.lon,
-        d.lat
-      ]) + ")";
-    })
-    .text(function(d) {
-      return d.city;
-    });
+  // CITY LABELS
 
   g.selectAll(".place-label")
     .data(cities)
@@ -200,14 +325,6 @@ d3.json("maps/states.json", function(error, us) {
     .attr("x", function(d) {
       return d.lon > -1 ? 6 : 8;
     })
-    // .style("text-anchor", function(d) {
-    // 	return d.lon > -1 ? "start" : "end";
-    // });
-
-  // g.append("path")
-  // 	.datum(topojson.feature(windData, windData.dwml.data))
-  // 	.attr("d", path)
-  // 	.attr("class", "place");
 
 
 
@@ -217,136 +334,137 @@ d3.json("maps/states.json", function(error, us) {
 
 
 
+  ///// USE this
+  // var windSpeed = waveData[4].data;
+  // var headers = waveData[4].header;
+  // var la1 = headers.la1;
+  // var lo1 = headers.lo1;
+  // var dx = headers.dx;
+  // var dy = headers.dy;
+  // var nx = headers.nx;
+  //
+  // var myWindData = windSpeed.map(function(d, i) {
+  //   var pos = projection([
+  //     lo1 + dx * (i % nx),
+  //     la1 + dy * parseInt(i / nx)
+  //   ]);
+  //   return {
+  //     t: d,
+  //     x: pos[0],
+  //     y: pos[1]
+  //   };
+  // })
+  // var color = d3.scale.linear()
+  //   .domain(d3.extent(windSpeed))
+  //   .range(["white", "black"]);
+  // var tempCircles = g.append('g').attr('class', 'temperatures');
+  // tempCircles.selectAll('line')
+  //   .data(myWindData.filter(function(d) {
+  //     if (isNaN(d.t)) {
+  //       return false;
+  //     } else {
+  //       return true;
+  //     };
+  //   }))
+  //   .enter()
+  //   .append("line")
+  //   .attr({
+  //     x1: function(d) {return d.x},
+  //     y1: function(d) {return d.y},
+  //     x2: function(d) {return (d.x+(5*d.t))},
+  //     y2: function(d) {return (d.y+(5*d.t))},
+  //   })
+  //   .attr("stroke-width", 2)
+  //   .attr("stroke", function(d) {
+  //     return color(d.t)
+  //   })
+  //   .on("click", clicked)
+  //
+  // .attr('r', 30)
+
+
+
+  // .attr("width", 10)
+  // .attr("height", 10)
+  // .attr('cx', function(d) {
+  //   return d.x;
+  // })
+  // .attr('cy', function(d) {
+  //   return d.y;
+  // })
+  // .style('fill', function(d) {
+  //   return color(d.t)
+  // })
+  // .call(lineAnimate);
 
 
 
 
 
 
-  console.log(waveData);
-  var windSpeed = waveData[4].data;
-  var headers = waveData[4].header;
-  var la1 = headers.la1;
-  var lo1 = headers.lo1;
-  var dx = headers.dx;
-  var dy = headers.dy;
-  var nx = headers.nx;
-
-  var myWindData = windSpeed.map(function(d, i) {
-    var pos = projection([
-      lo1 + dx * (i % nx),
-      la1 + dy * parseInt(i / nx)
-    ]);
-    return {
-      t: d,
-      x: pos[0],
-      y: pos[1]
-    };
-  })
-  var color = d3.scale.linear()
-    .domain(d3.extent(windSpeed))
-    .range(["white", "black"]);
-  var tempCircles = g.append('g').attr('class', 'temperatures');
-  tempCircles.selectAll('line')
-    .data(myWindData.filter(function(d) {
-      if (isNaN(d.t)) {
-        return false;
-      } else {
-        return true;
-      };
-    }))
-    .enter()
-    .append("line")
-    .attr({
-      x1: function(d) {return d.x},
-      y1: function(d) {return d.y},
-      x2: function(d) {return (d.x+(5*d.t))},
-      y2: function(d) {return (d.y+(5*d.t))},
-    })
-    .attr("stroke-width", 2)
-    .attr("stroke", function(d) {
-      return color(d.t)
-    })
-    .on("click", clicked)
-    // .attr('r', 30)
-    // // .attr("width", 10)
-    // // .attr("height", 10)
-    // .attr('cx', function(d) {
-    //   return d.x;
-    // })
-    // .attr('cy', function(d) {
-    //   return d.y;
-    // })
-    // .style('fill', function(d) {
-    //   return color(d.t)
-    // })
-    // .call(lineAnimate);
 
 
 
-
-// var lines = []
-// 	//// MATH FUNCTIONS
-// 	function toRad(deg) {return deg * Math.PI / 180;}
-// 	function toDeg(rad) {return rad * 180 / Math.PI;}
-//
-// 	function lonLatFromLonLatDistanceAndBearing(lonLat, d, brng) {
-// 	  // Formulae from http://www.movable-type.co.uk/scripts/latlong.html
-// 	  // brg in radians, d in km
-// 	  var R = 6371; // Earth's radius in km
-// 	  var lon1 = toRad(lonLat[0]), lat1 = toRad(lonLat[1]);
-// 	  var lat2 = Math.asin( Math.sin(lat1)*Math.cos(d/R) + Math.cos(lat1)*Math.sin(d/R)*Math.cos(brng) );
-// 	  var lon2 = lon1 + Math.atan2(Math.sin(brng)*Math.sin(d/R)*Math.cos(lat1), Math.cos(d/R)-Math.sin(lat1)*Math.sin(lat2));
-// 	  return [toDeg(lon2), toDeg(lat2)];
-// 	}
-//
-// 	//// INITIALISATION
-// 	var cardinalToBearing = {};
-//
-// 	function init() {
-// 	  var i, cardinalPoints = ['S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE'];
-// 	  // Calculate cardinal point to bearing mapping (wind direction is where the wind is coming *from*!)
-// 	  for(i = 0; i < cardinalPoints.length; i++)
-// 	    cardinalToBearing[cardinalPoints[i]] = i * Math.PI / 8;
-// 	  // Prepare line co-ordinates
-// 	  var windData = weather.SiteRep.DV.Location;
-// 	  for(i = 0; i < windData.length; i++) {
-// 	    var d = windData[i];
-// 	    var speed = d.Period.Rep.S;
-// 	    var feelsLikeTemperature = d.Period.Rep.F;
-// 	    var lonLat0 = [d.lon, d.lat];
-//
-// 	    // Scale line length proportionally to speed
-// 	    var lonLat1 = lonLatFromLonLatDistanceAndBearing(lonLat0, 1.2 * speed, cardinalToBearing[d.Period.Rep.D]);
-//
-// 	    var x0y0 = projection(lonLat0);
-// 	    var x1y1 = projection(lonLat1);
-// 	    var line = {
-// 	      x0: x0y0[0],
-// 	      y0: x0y0[1],
-// 	      x1: x1y1[0],
-// 	      y1: x1y1[1],
-// 	      s: speed,
-// 	      // f: feelsLikeTemperature,
-// 	      duration: 8000 / speed, /* pre-compute duration */
-// 	      delay: Math.random() * 1000 /* pre-compute delay */
-// 	    };
-// 	    // console.log(line);
-// 	    lines.push(line);
-// 	  }
-// 	}
-//
-// 	g.selectAll("line")
-// 	  .data(lines)
-// 	  .enter()
-// 	  .append("line")
-// 	  .attr({
-// 	    x1: function(d) {return d.x0},
-// 	    y1: function(d) {return d.y0}
-// 	  })
-// 	  .call(lineAnimate);
-
-
+  // var lines = []
+  // 	//// MATH FUNCTIONS
+  // 	function toRad(deg) {return deg * Math.PI / 180;}
+  // 	function toDeg(rad) {return rad * 180 / Math.PI;}
+  //
+  // 	function lonLatFromLonLatDistanceAndBearing(lonLat, d, brng) {
+  // 	  // Formulae from http://www.movable-type.co.uk/scripts/latlong.html
+  // 	  // brg in radians, d in km
+  // 	  var R = 6371; // Earth's radius in km
+  // 	  var lon1 = toRad(lonLat[0]), lat1 = toRad(lonLat[1]);
+  // 	  var lat2 = Math.asin( Math.sin(lat1)*Math.cos(d/R) + Math.cos(lat1)*Math.sin(d/R)*Math.cos(brng) );
+  // 	  var lon2 = lon1 + Math.atan2(Math.sin(brng)*Math.sin(d/R)*Math.cos(lat1), Math.cos(d/R)-Math.sin(lat1)*Math.sin(lat2));
+  // 	  return [toDeg(lon2), toDeg(lat2)];
+  // 	}
+  //
+  // 	//// INITIALISATION
+  // 	var cardinalToBearing = {};
+  //
+  // 	function init() {
+  // 	  var i, cardinalPoints = ['S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE'];
+  // 	  // Calculate cardinal point to bearing mapping (wind direction is where the wind is coming *from*!)
+  // 	  for(i = 0; i < cardinalPoints.length; i++)
+  // 	    cardinalToBearing[cardinalPoints[i]] = i * Math.PI / 8;
+  // 	  // Prepare line co-ordinates
+  // 	  var windData = weather.SiteRep.DV.Location;
+  // 	  for(i = 0; i < windData.length; i++) {
+  // 	    var d = windData[i];
+  // 	    var speed = d.Period.Rep.S;
+  // 	    var feelsLikeTemperature = d.Period.Rep.F;
+  // 	    var lonLat0 = [d.lon, d.lat];
+  //
+  // 	    // Scale line length proportionally to speed
+  // 	    var lonLat1 = lonLatFromLonLatDistanceAndBearing(lonLat0, 1.2 * speed, cardinalToBearing[d.Period.Rep.D]);
+  //
+  // 	    var x0y0 = projection(lonLat0);
+  // 	    var x1y1 = projection(lonLat1);
+  // 	    var line = {
+  // 	      x0: x0y0[0],
+  // 	      y0: x0y0[1],
+  // 	      x1: x1y1[0],
+  // 	      y1: x1y1[1],
+  // 	      s: speed,
+  // 	      // f: feelsLikeTemperature,
+  // 	      duration: 8000 / speed, /* pre-compute duration */
+  // 	      delay: Math.random() * 1000 /* pre-compute delay */
+  // 	    };
+  // 	    // console.log(line);
+  // 	    lines.push(line);
+  // 	  }
+  // 	}
+  //
+  // 	g.selectAll("line")
+  // 	  .data(lines)
+  // 	  .enter()
+  // 	  .append("line")
+  // 	  .attr({
+  // 	    x1: function(d) {return d.x0},
+  // 	    y1: function(d) {return d.y0}
+  // 	  })
+  // 	  .call(lineAnimate);
 
 
 
@@ -376,25 +494,38 @@ var tickFormatter = function(d) {
 }
 
 
-// line animation
-function lineAnimate(selection) {
-    selection
-    .attr({x1: 200, x2: 200})
-    .attr('y1', function(d) {return d;})
-    .attr('y2', function(d) {return d;})
-    .style('opacity', 0.5)
-    .transition()
-        .ease('linear')
-        .duration(1000)
-        .delay(function(d) {return d*10;})
-        .attr('x2', 500)
-    .transition()
-        .duration(1000)
-        .style('opacity', 0)
-    .each('end', function() {d3.select(this).call(lineAnimate)});
-}
+// LINE ANIMATION
 
-// Initialize slider
-var slider = d3.slider().min(0).max(20).tickValues([0, 5, 10, 15, 20]).stepValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]).tickFormat(tickFormatter);
-// Render the slider in the div
-d3.select('#slider').call(slider);
+function lineAnimate(selection) {
+  selection
+    .attr({
+      x2: function(d) {return d.lat},
+      y2: function(d) {return d.lon}
+    })
+    .style('opacity', 0)
+    .transition()
+    .ease('linear')
+    .duration(1000)
+    .delay(function(d) {
+      return d.spd[0] * 10;
+    })
+    .attr({
+        x2: function(d) {return (d.lat + (parseInt(d.spd[0])))},
+        y2: function(d) {return (d.lon + (parseInt(d.spd[0])))}
+        })
+      .style('opacity', 0.9)
+      .transition()
+      .duration(1000)
+      .style('opacity', 0.1)
+      .each('end', function() {
+        d3.select(this).call(lineAnimate)
+      });
+    }
+
+
+
+
+  // Initialize slider
+  var slider = d3.slider().min(0).max(20).tickValues([0, 5, 10, 15, 20]).stepValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]).tickFormat(tickFormatter);
+  // Render the slider in the div
+  d3.select('#slider').call(slider);
